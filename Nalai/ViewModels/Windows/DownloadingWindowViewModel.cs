ad554e9d56348154874e0b04a67906d9d4876d33
sout233 <2273466101@qq.com>
2024-10-23 18:48:35 +0800

    update downloading window LAYOUT

--- Nalai/ViewModels/Windows/DownloadingWindowViewModel.cs
+++ Nalai/ViewModels/Windows/DownloadingWindowViewModel.cs
@@ -1,8 +1,9 @@
-﻿using Nalai.Models;
+﻿using System.Collections.ObjectModel;
+using Nalai.Models;
 
 namespace Nalai.ViewModels.Windows;
 
-public partial class 
+public partial class
     DownloadingWindowViewModel : ObservableObject
 {
     [ObservableProperty] private string _applicationTitle = "Downloading...";
@@ -14,25 +15,46 @@ public partial class
     [ObservableProperty] private string _fileSize = "Unknown";
     [ObservableProperty] private string _remainingTime = "Unknown";
     [ObservableProperty] private IEnumerable<ChunkValue> _chunkValues = new List<ChunkValue>();
+    [ObservableProperty] private ObservableCollection<ProgressData> _progressBars=new();
 
+    public DownloadingWindowViewModel()
+    {
+        AddProgressBars();
+    }
+    public class ProgressData
+    {
+        public int Value { get; set; }
+        public int Maximum { get; set; }
+    }
+    public void AddProgressBars()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            ProgressBars.Add(new ProgressData { Value = 0, Maximum = 100 });
+        }
+    }
+
+    
     public void SetProgress(double value)
     {
         ProgressValue = value;
     }
-    
+
     public void SetFileName(string? value)
     {
         FileName = value;
     }
+
     public void SetRemainingTime(string value)
     {
         RemainingTime = value;
     }
+
     public void SetFileSize(string value)
     {
         FileSize = value;
     }
-    
+
     public void SetDownloadSpeed(string value)
     {
         DownloadSpeed = value;
@@ -42,7 +64,7 @@ public partial class
     {
         ProgressText = value;
     }
-    
+
     public void SetChunkValues(IEnumerable<ChunkValue> values)
     {
         ChunkValues = values;
