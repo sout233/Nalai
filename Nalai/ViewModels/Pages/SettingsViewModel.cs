872faccf65983f9b22dfad770e4fa0f7cb6f7b43
sout233 <2273466101@qq.com>
2024-10-29 15:43:36 +0800

    Change about name

--- Nalai/ViewModels/Pages/SettingsViewModel.cs
+++ Nalai/ViewModels/Pages/SettingsViewModel.cs
@@ -24,7 +24,7 @@ namespace Nalai.ViewModels.Pages
         private void InitializeViewModel()
         {
             CurrentTheme = ApplicationThemeManager.GetAppTheme();
-            AppVersion = $"UiDesktopApp1 - {GetAssemblyVersion()}";
+            AppVersion = $"Nalai - {GetAssemblyVersion()}";
 
             _isInitialized = true;
         }
