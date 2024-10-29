91b6eb7f4c3ab34684214f7eacdebf0d409a667d
sout233 <2273466101@qq.com>
2024-10-29 09:14:36 +0800

    Add download complete window view model and change layout

--- Nalai/ViewModels/Windows/DownloadCompleteWindowViewModel.cs
+++ Nalai/ViewModels/Windows/DownloadCompleteWindowViewModel.cs
@@ -1,6 +1,18 @@
-﻿namespace Nalai.ViewModels.Windows;
+﻿using Nalai.Views.Windows;
+
+namespace Nalai.ViewModels.Windows;
 
 public partial class DownloadCompleteWindowViewModel:ObservableObject
 {
+    public required DownloadCompleteWindow BindWindow { get; set; }
+    
     [ObservableProperty] private string _applicationTitle="Download Complete";
+    [ObservableProperty] private string _fileName = "Unknown";
+    [ObservableProperty] private string _downloadPath = "Unknown";
+    
+    [RelayCommand]
+    private void CloseWindow()
+    {
+        BindWindow.Close();
+    }
 }
