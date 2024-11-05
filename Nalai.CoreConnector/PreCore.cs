1fafeedbaeee29cf86a4d3d6fc93238de23e94fe
sout_Nantang <55623894+sout233@users.noreply.github.com>
2024-11-05 12:40:29 +0800

    Add: Core Connector

---
+++ Nalai.CoreConnector/PreCore.cs
@@ -0,0 +1,29 @@
+﻿namespace Nalai.CoreConnector;
+
+public class PreCore
+{
+    public static async Task DownloadFile()
+    {
+    using (var client = new HttpClient()) {
+        try
+        {
+            var response = await client.GetAsync("http://127.0.0.1:3030/download");
+            response.EnsureSuccessStatusCode();
+            using (var stream = await response.Content.ReadAsStreamAsync())
+            using (var fileStream =
+                   new FileStream("downloaded_file.zip", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                await stream.CopyToAsync(fileStream);
+            }
+
+            Console.WriteLine("下载完成！");
+        }
+        catch (HttpRequestException e)
+        {
+            Console.WriteLine($"请求错误: {e.Message}");
+        }
+    }
+
+    }
+    
+}
