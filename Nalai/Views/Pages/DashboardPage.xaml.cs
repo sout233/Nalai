599da08ddf3c32d90c1ee667e6323237d2d04046
sout233 <2273466101@qq.com>
2024-11-25 19:40:51 +0800

    🎈 perf: improve search

--- Nalai/Views/Pages/DashboardPage.xaml.cs
+++ Nalai/Views/Pages/DashboardPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Windows.Input;
+﻿using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 using Nalai.Models;
 using Nalai.ViewModels.Pages;
 using Wpf.Ui.Controls;
+using ListViewItem = System.Windows.Controls.ListViewItem;
 
 namespace Nalai.Views.Pages;
 
@@ -15,6 +18,8 @@ public partial class DashboardPage : INavigableView<DashboardViewModel>
         DataContext = this;
 
         InitializeComponent();
+        CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(DownloadTaskListView.ItemsSource);
+        view.Filter = UserFilter;
     }
 
     private void ContextMenu_OnOpened(object sender, RoutedEventArgs e)
@@ -45,5 +50,20 @@ public partial class DashboardPage : INavigableView<DashboardViewModel>
 
         ViewModel.UpdatePauseOrResumeElement(task.Status);
         ViewModel.SetPauseOrResumeButtonEnabled(true);
+    }
+    
+    private bool UserFilter(object item)
+    {
+        if(String.IsNullOrEmpty(SearchTextBox.Text))
+            return true;
+        else
+            return ((item as CoreTask).FileName.IndexOf(SearchTextBox.Text, StringComparison.OrdinalIgnoreCase) >= 0);
+    }
+
+    private void SearchTextBox_OnTextChanged(object sender, TextChangedEventArgs e)
+    {
+        CollectionViewSource.GetDefaultView(DownloadTaskListView.ItemsSource).Refresh();
+
+        Console.WriteLine("SearchTextBox_OnTextChanged");
     }
 }
