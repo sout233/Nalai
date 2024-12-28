9b7f6de29c8320c0f5d4bfa974b5e6c69d7cfd11
sout233 <2273466101@qq.com>
2024-12-28 19:45:05 +0800

    🦄 refactor: split the native msg config reg into separate methods
    just for Firefox and Chrome

--- Nalai/Helpers/RegManager.cs
+++ Nalai/Helpers/RegManager.cs
@@ -7,20 +7,44 @@ namespace Nalai.Helpers;
 
 public class RegManager
 {
-    public static void RegisterNativeMessagingConfig()
+    private const string FirefoxExtensionId = "nmmnkjnkobkdbjlopninpkddchcbnbkcf";
+    private const string ChromeExtensionId = "chrome-extension://nhliacfehfhgideomolhjnnfabmmeiag/";
+
+    public static void RegisterChromeNativeMessagingConfig()
     {
         var config = new NativeMessagingConfig("org.eu.sout.nalai", "A native messaging host for Nalai",
-            Environment.CurrentDirectory + @"\Nalai.exe", "stdio", ["https://*.sout.eu.org/*"]);
-        var configFilePath = Environment.CurrentDirectory + @"\org.eu.sout.nalai.json";
+            Path.Combine(Environment.CurrentDirectory, "Nalai.exe"), "stdio", [ChromeExtensionId]);
+        var configFilePath = Environment.CurrentDirectory + @"\Configs\org.eu.sout.nalai_chrome.json";
 
+        if (!File.Exists(configFilePath))
+        {
+            Directory.CreateDirectory(Environment.CurrentDirectory + @"\Configs");
+            File.Create(configFilePath).Close();
+        }
+        
         File.WriteAllText(configFilePath, JsonConvert.SerializeObject(config, Formatting.Indented));
         
         var regLoc = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Google\Chrome\NativeMessagingHosts\org.eu.sout.nalai", true) ??
                      Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Google\Chrome\NativeMessagingHosts\org.eu.sout.nalai");
         regLoc.SetValue(null, configFilePath);
+    }
+
+    public static void RegisterFirefoxNativeMessagingConfig()
+    {
+        var config = new NativeMessagingConfig("org.eu.sout.nalai", "A native messaging host for Nalai",
+            Path.Combine(Environment.CurrentDirectory, "Nalai.exe"), "stdio", [FirefoxExtensionId]);
+        var configFilePath = Environment.CurrentDirectory + @"\Configs\org.eu.sout.nalai_firefox.json";
+
+        if (!File.Exists(configFilePath))
+        {
+            Directory.CreateDirectory(Environment.CurrentDirectory + @"\Configs");
+            File.Create(configFilePath).Close();
+        }
         
-        regLoc = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Mozilla\NativeMessagingHosts\org.eu.sout.nalai", true) ?? 
-                 Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Mozilla\NativeMessagingHosts\org.eu.sout.nalai");
+        File.WriteAllText(configFilePath, JsonConvert.SerializeObject(config, Formatting.Indented));
+        
+        var regLoc = Registry.CurrentUser.OpenSubKey(@"Software\Mozilla\NativeMessagingHosts\org.eu.sout.nalai", true) ??
+                     Registry.CurrentUser.CreateSubKey(@"Software\Mozilla\NativeMessagingHosts\org.eu.sout.nalai");
         regLoc.SetValue(null, configFilePath);
     }
 }
