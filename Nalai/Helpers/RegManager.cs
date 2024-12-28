17ffdfe779a1944794119d78a6c41d3c5d8c4a38
sout233 <2273466101@qq.com>
2024-12-28 10:09:53 +0800

    ✨ feat: add native messaging model and manager

---
+++ Nalai/Helpers/RegManager.cs
@@ -0,0 +1,12 @@
+﻿using Nalai.Models;
+
+namespace Nalai.Helpers;
+
+public class RegManager
+{
+    public static void RegisterNativeMessagingConfig()
+    {
+        var config = new NativeMessagingConfig("org.eu.sout.nalai", "A native messaging host for Nalai",
+            Environment.CurrentDirectory + @"\Nalai.exe", "stdio", ["https://*.sout.eu.org/*"]);
+    }
+}
---
+++ Nalai/Models/NativeMessagingConfig.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Nalai.Models;
+
+public record NativeMessagingConfig(
+    string Name,
+    string Description,
+    string Path,
+    string Type,
+    List<string> AllowedOrigins)
+{
+    [JsonProperty("name")]
+    public string Name { get; set; } = Name;
+
+    [JsonProperty("description")]
+    public string Description { get; set; } = Description;
+
+    [JsonProperty("path")]
+    public string Path { get; set; } = Path;
+
+    [JsonProperty("type")]
+    public string Type { get; set; } = Type;
+
+    [JsonProperty("allowed_origins")]
+    public List<string> AllowedOrigins { get; set; } = AllowedOrigins;
+}
