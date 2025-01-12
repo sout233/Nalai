5c590bff578ef2c43604b4df8ff59ddec5889b90
sout233 <2273466101@qq.com>
2025-01-12 10:01:39 +0800

    ✨ feat: add SettingsConfig model

---
+++ Nalai/Models/SettingsConfig.cs
@@ -0,0 +1,27 @@
+﻿using Wpf.Ui.Appearance;
+
+namespace Nalai.Models;
+
+public class SettingsConfig
+{
+    public SettingGeneral General { get; set; }
+    public SettingAppearance Appearance { get; set; }
+    public SettingDownload Download { get; set; }
+}
+
+public class SettingGeneral
+{
+    public string Language { get; set; }
+    public bool isStartWithWindows { get; set; }
+    public bool isUseSlientMode { get; set; }
+}
+
+public class SettingAppearance
+{
+    public ApplicationTheme Theme { get; set; }
+}
+
+public class SettingDownload
+{
+    public bool isShowCompletedWindow { get; set; }
+}
